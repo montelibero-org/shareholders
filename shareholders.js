c4d2930af74f2a0c3f98dc5728a41c99aa5a3f74
Yuriy Syrovetskiy <git-orbi@cblp.su>
2021-06-07 12:37:53 +0300

    Refactor in async

--- shareholders.js
+++ shareholders.js
@@ -6,97 +6,132 @@ const MTL_TREASURY = 'GDX23CPGMQ4LN55VGEDVFZPAJMAUEHSHAMJ2GMCU2ZSHN5QF4TMZYPIS';
 const MTLCITY_CODE = 'MTLCITY';
 const MTLCITY_ISSUER = 'GDUI7JVKWZV4KJVY4EJYBXMGXC2J3ZC67Z6O5QFP4ZMVQM2U5JXK2OK3';
 
-const MTL     = new StellarSdk.Asset(MTL_CODE,     MTL_ISSUER);
+const MTL = new StellarSdk.Asset(MTL_CODE, MTL_ISSUER);
+MTL.fetch_limit = 100;
+
 const MTLCITY = new StellarSdk.Asset(MTLCITY_CODE, MTLCITY_ISSUER);
+MTLCITY.fetch_limit = 100;
 
 const server = new StellarSdk.Server('https://horizon.stellar.org');
 
-function collectPages(asset, onPage, atEnd) {
-  let collector = {
-    asset: asset,
-    holders: [],
-    supply: 0,
-    distributed: 0,
-  };
-  function recur(collectionPage) {
-    if (collectionPage.records.length) {
-      collectHolders(collector, collectionPage.records);
-      onPage(collector, collectionPage.records);
-      collectionPage.next().then(recur);
-    } else {
-      // end of collection
-      atEnd(collector);
-    }
-  };
-  return recur;
-}
+async function getFundInfo(asset) {
+  const holders =
+    await
+      server
+        .accounts()
+        .forAsset(asset)
+        .limit(asset.fetch_limit)
+        .call()
+        .then(({records}) => records);
 
-function collectHolders(collector, records) {
-  collector.holders = collector.holders.concat(records);
-  for (const accountRecord of records) {
+  let supply = 0;
+  let distributed = 0;
+  let mtl_treasury_balance = 0;
+  for (const accountRecord of holders) {
     const [balance] =
       accountRecord
       .balances
       .filter(
         ({asset_code, asset_issuer}) =>
-          asset_code   == collector.asset.getCode() &&
-          asset_issuer == collector.asset.getIssuer()
+          asset_code   == asset.getCode() &&
+          asset_issuer == asset.getIssuer()
       )
       .map(({balance}) => +balance);
     accountRecord.balance = balance;
 
-    collector.supply += balance;
+    supply += balance;
 
-    if (accountRecord.account_id != MTL_TREASURY || collector.asset != MTL)
-      collector.distributed += balance;
+    if (accountRecord.account_id != MTL_TREASURY || asset != MTL)
+      distributed += balance;
     if (accountRecord.account_id == MTL_TREASURY)
-      collector.mtl_treasury_balance = balance;
+      mtl_treasury_balance = balance;
   }
-}
 
-function appendHolders(supply_text, distributed_text) {
-  return (collector, records) => {
-    for (const accountRecord of records) {
-      supply_text.innerText = collector.supply;
-      if (distributed_text)
-        distributed_text.innerText = collector.distributed;
-    }
+  for (const accountRecord of holders) {
+    accountRecord.share = accountRecord.balance / distributed;
+    accountRecord.power = accountRecord.share;
+  }
+
+  return {
+    asset: asset,
+    distributed: distributed,
+    holders: holders,
+    mtl_treasury_balance: mtl_treasury_balance,
+    supply: supply,
   };
 }
 
-function appendHoldersTableRow(collector, table, accountRecord) {
+function setHoldersStats(fundInfo, supply_text, distributed_text) {
+  supply_text.innerText = fundInfo.supply;
+  if (distributed_text)
+    distributed_text.innerText = fundInfo.distributed;
+}
+
+function mergeMtlHolders(fundInfo, parentFundInfo) {
+  let parentShares = {};
+  for (const accountRecord of parentFundInfo.holders) {
+    parentShares[accountRecord.account_id] =
+      accountRecord.balance / parentFundInfo.distributed;
+  }
+
+  let holderDict = {};
+  for (const accountRecord of fundInfo.holders) {
+    if (accountRecord.account_id in parentShares) {
+      accountRecord.parentShare = parentShares[accountRecord.account_id];
+      accountRecord.power +=
+        accountRecord.parentShare
+        * fundInfo.mtl_treasury_balance
+        / fundInfo.distributed;
+    }
+    holderDict[accountRecord.account_id] = accountRecord;
+  }
+
+  // append parent holders
+  for (const accountRecord of parentFundInfo.holders) {
+    if (!(accountRecord.account_id in holderDict)) {
+      accountRecord.balance = 0;
+      accountRecord.share = 0;
+      accountRecord.parentShare = parentShares[accountRecord.account_id];
+      accountRecord.power =
+        accountRecord.parentShare
+          * parentFundInfo.mtl_treasury_balance
+          / parentFundInfo.distributed;
+      holderDict[accountRecord.account_id] = accountRecord;
+    }
+  }
+
+  fundInfo.holders = Object.values(holderDict);
+}
+
+function appendHoldersTableRow(fundInfo, table, accountRecord) {
   const name =
     accountRecord.account_id == MTL_TREASURY
     ? 'MTL Treasury'
     : '<a href="https://stellar.expert/explorer/public/account/' + accountRecord.account_id + '" rel="nofollow noreferrer noopener" target="_blank">' + accountRecord.account_id + '</a>';
 
-  const balance =
-    accountRecord.account_id == MTL_TREASURY
-    ? ''
-    : accountRecord.balance;
-
   // TODO show the final power of the vote given the participation in the MTL and the delegation transactions
-  // TODO if vote power is less than 0.01 then show ">0.01%"
-  // TODO dont show accounts with 0% vote powers
-  const vote_power = (accountRecord.power * 100).toFixed(2) + '%';
+  // TODO if vote power is less than 0.01 then show "<0.01%"
+  // TODO don't show accounts with 0 vote power
+  const power = (accountRecord.power * 100).toFixed(2) + '%';
+
   // TODO remove this column later
-  const explanation =
-    accountRecord.account_id == MTL_TREASURY
-    ? ''
-    : `share in ${collector.asset.getCode()} =
-        ${accountRecord.share * 100}%` +
-    (accountRecord.parent_share
-      ? `, share in MTL = ${accountRecord.parent_share * 100}%,
-        MTL balance in ${collector.asset.getCode()} =
-          ${collector.mtl_treasury_balance},
-        MTL share in ${collector.asset.getCode()} =
-          ${collector.mtl_treasury_balance / collector.distributed * 100}%,
-        share via MTL =
-          ${accountRecord.parent_share
-            * collector.mtl_treasury_balance
-            / collector.distributed
-            * 100}%`
-      : '');
+  let explanation = '';
+  if (accountRecord.account_id != MTL_TREASURY)
+    explanation +=
+      `balance = ${accountRecord.balance} ${fundInfo.asset.getCode()},
+      share in ${fundInfo.asset.getCode()} = ${accountRecord.share * 100}%`;
+  if (accountRecord.parentShare)
+    explanation +=
+      `, share in MTL = ${accountRecord.parentShare * 100}%,
+      MTL balance in ${fundInfo.asset.getCode()} =
+        ${fundInfo.mtl_treasury_balance},
+      MTL share in ${fundInfo.asset.getCode()} =
+        ${fundInfo.mtl_treasury_balance / fundInfo.distributed * 100}%,
+      share via MTL =
+        ${accountRecord.parentShare
+          * fundInfo.mtl_treasury_balance
+          / fundInfo.distributed
+          * 100}%`;
 
   const tr = table.appendChild(document.createElement('tr'));
   // TODO calculate Rank for accounts, starts from 1
@@ -104,68 +139,24 @@ function appendHoldersTableRow(collector, table, accountRecord) {
     .appendChild(document.createTextNode('0'));
   // TODO show it only to signers of issuer
   tr.appendChild(document.createElement('td'))
-    .appendChild(document.createTextNode(vote_power));
+    .appendChild(document.createTextNode(power));
   tr.appendChild(document.createElement('td'))
     .appendChild(document.createTextNode('S'));
   // TODO output HTML code correctly for a links
   tr.appendChild(document.createElement('td'))
     .appendChild(document.createTextNode(name));
   tr.appendChild(document.createElement('td'))
-    .appendChild(document.createTextNode(balance));
+    .appendChild(document.createTextNode(accountRecord.balance));
   tr.appendChild(document.createElement('td'))
     .appendChild(document.createTextNode(explanation));
 }
 
-function makeHoldersTable(table, parent_collector) {
-  let parent_shares = {};
-  if (parent_collector) {
-    for (const accountRecord of parent_collector.holders) {
-      parent_shares[accountRecord.account_id] =
-        accountRecord.balance / parent_collector.distributed;
-    }
+function makeHoldersTable(fundInfo, table) {
+  // sort by .power descending
+  fundInfo.holders.sort((a, b) => b.power - a.power);
+
+  for (const accountRecord of fundInfo.holders) {
+    if (accountRecord.account_id != MTL_TREASURY)
+      appendHoldersTableRow(fundInfo, table, accountRecord);
   }
-
-  return collector => {
-    let holders = {};
-
-    for (const accountRecord of collector.holders) {
-      accountRecord.share = accountRecord.balance / collector.distributed;
-      accountRecord.parent_share =
-        accountRecord.account_id in parent_shares
-        ? parent_shares[accountRecord.account_id]
-        : 0;
-      accountRecord.power =
-        accountRecord.share
-        + accountRecord.parent_share
-          * collector.mtl_treasury_balance
-          / collector.distributed;
-      holders[accountRecord.account_id] = accountRecord;
-    }
-
-    // append parent holders
-    if (parent_collector) {
-      for (const accountRecord of parent_collector.holders) {
-        if (!(accountRecord.account_id in holders)) {
-          accountRecord.balance = 0;
-          accountRecord.share = 0;
-          accountRecord.parent_share = parent_shares[accountRecord.account_id];
-          accountRecord.power =
-            accountRecord.parent_share
-              * collector.mtl_treasury_balance
-              / collector.distributed;
-          holders[accountRecord.account_id] = accountRecord;
-        }
-      }
-    }
-
-    holders = Object.values(holders);
-
-    // sort by .power descending
-    holders.sort((a, b) => b.power - a.power);
-
-    for (const accountRecord of holders) {
-      if (accountRecord.account_id != MTL_TREASURY)
-        appendHoldersTableRow(collector, table, accountRecord);
-    }
-  };
 }
