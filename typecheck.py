7e781000e576416503a14c7c3f8bd01ebac9b394
Yuriy Syrovetskiy <git-orbi@cblp.su>
2021-06-14 15:15:42 +0300

    Switch typechecker runner to Python

---
+++ typecheck.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from os import path
+from bs4 import BeautifulSoup
+from subprocess import run
+from tempfile import TemporaryDirectory
+
+html = open('index.html').read()
+parsed_html = BeautifulSoup(html, features='html.parser')
+script_tag = parsed_html.body.find(id='main')
+[script] = script_tag.children
+
+with TemporaryDirectory() as dir:
+    ts = path.join(dir, 'main.ts')
+    with open(ts, 'w') as f:
+        f.write('\n' * (script_tag.sourceline - 1))
+        f.write(script)
+    run(['tsc', '--noEmit', '--strict', ts])
--- typecheck.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eux -o pipefail
-
-TMPFILE=/tmp/typecheck.$RANDOM
-xmlstarlet select --text --template --value-of '//body//script' index.html \
-  > "$TMPFILE.ts"
-tsc --noEmit --strict "$TMPFILE.ts"
-rm "$TMPFILE.*"
