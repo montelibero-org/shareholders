f9f5af05340a5d35466277e6d5a244f4ecabf0e9
Yuriy Syrovetskiy <git-orbi@cblp.su>
2021-08-07 21:31:57 +0300

    Add fetching payments till the end (#12)

--- typecheck.py
+++ typecheck.py
@@ -2,8 +2,9 @@
 
 from os import path
 from bs4 import BeautifulSoup
-from subprocess import CalledProcessError, check_call
+from subprocess import run
 from tempfile import TemporaryDirectory
+from sys import stderr, stdout
 
 html = open('index.html').read()
 parsed_html = BeautifulSoup(html, features='html.parser')
@@ -12,10 +13,21 @@ script_tag = parsed_html.body.find(id='main')
 
 with TemporaryDirectory() as dir:
     ts = path.join(dir, 'main.ts')
+    rel = path.relpath(ts)
     with open(ts, 'w') as f:
         f.write('\n' * (script_tag.sourceline - 1))
         f.write(script)
-    try:
-        check_call(['tsc', '--lib', 'es2017,dom', '--noEmit', '--strict', '--target', 'ES5', ts])
-    except CalledProcessError as e:
-        exit(e.returncode)
+    p = run(
+        ['tsc',
+         '--lib', 'es2017,dom',
+         '--noEmit',
+         '--pretty',
+         '--strict',
+         '--target', 'ES5',
+         ts,
+         ],
+        capture_output=True,
+    )
+    stdout.write(p.stdout.decode().replace(rel, './index.html'))
+    stderr.write(p.stderr.decode())
+    exit(p.returncode)
